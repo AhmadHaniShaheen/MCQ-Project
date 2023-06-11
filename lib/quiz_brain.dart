d3cb5f7b8c89cba8f64459dadba9b25b4467799a
ahmadhanialishaheen@gmail.com <ahmadhanialishaheen@gmail.com>
2023-06-11 15:58:17 +0300

    step-5

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -6,17 +6,49 @@ class QuizBrain {
   final List<Questions> _questionBank = [
     Questions(
       questionText:
-          "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat",
+          "1- In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat",
       answer: true,
     ),
     Questions(
       questionText:
-          "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog",
+          "2- Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog",
       answer: false,
     ),
     Questions(
-      questionText: "Google was originally called \"Backrub\"",
+      questionText: "3- Google was originally called \"Backrub\"",
       answer: false,
+    ),
+    Questions(
+      questionText:
+          "4- Is it legal to take home an animal you accidentally hit with your car in West Virginia, USA?",
+      answer: true,
+    ),
+    Questions(
+      questionText:
+          "5- Can a person survive without drinking water for a week?",
+      answer: false,
+    ),
+    Questions(
+      questionText:
+          "6- Is English the most widely spoken language in the world?",
+      answer: false,
+    ),
+    Questions(
+      questionText: "7- Does the sun revolve around the Earth?",
+      answer: false,
+    ),
+    Questions(
+      questionText: "8- Is the Great Wall of China visible from space?",
+      answer: false,
+    ),
+    Questions(
+      questionText:
+          "9- Is it possible for humans to breathe underwater without any assistance?",
+      answer: false,
+    ),
+    Questions(
+      questionText: "10- Is Mount Everest the tallest mountain in the world?",
+      answer: true,
     ),
   ];
 
